81f81c0c8264c5dffb024472596f5648bf030a36
sssinaaa <sina.shanibeik@gmail.com>
2019-11-12 21:13:16 +0330

    Updates

---
+++ precache-manifest.0bd89c577e73367395b0641cd2e06cef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfb1ab4f7db26e85328b5e317101819",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "211236daaa464b170366",
+    "url": "/robofriends/static/css/main.1927d792.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "211236daaa464b170366",
+    "url": "/robofriends/static/js/main.5f3e76d0.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
